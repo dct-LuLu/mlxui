2031d8fc294a0482e7b8a846098ab5a5c720de57
jaubry-- <jaubry--@student.42lyon.fr>
2025-12-28 19:20:16 +0100

    FIX: inverted controls for scrlling in scrollbox

--- src/components/scrollbox/mouse_hooks_scrollbox.c
+++ src/components/scrollbox/mouse_hooks_scrollbox.c
@@ -6,7 +6,7 @@
 /*   By: jaubry-- <jaubry--@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 01:34:13 by jaubry--          #+#    #+#             */
-/*   Updated: 2025/12/23 22:17:27 by jaubry--         ###   ########.fr       */
+/*   Updated: 2025/12/28 19:18:11 by jaubry--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,9 @@ void	hook_scrollup_scrollbox(t_vec2i pos, t_maction action,
 	if ((action == MPRESS) && hbranch->visible
 		&& is_inside_comp(hbranch, pos))
 	{
-		hbranch->scrollbox._current_pos += hbranch->scrollbox.sensitivity;
-		if (hbranch->scrollbox._current_pos
-			> (hbranch->scrollbox._scroll_buffer.height - hbranch->size.y))
-			hbranch->scrollbox._current_pos
-				= hbranch->scrollbox._scroll_buffer.height - hbranch->size.y;
+		hbranch->scrollbox._current_pos -= hbranch->scrollbox.sensitivity;
+		if (hbranch->scrollbox._current_pos < 0)
+			hbranch->scrollbox._current_pos = 0;
 		update_scrollbar(hbranch);
 	}
 }
@@ -47,9 +45,11 @@ void	hook_scrolldown_scrollbox(t_vec2i pos, t_maction action,
 	(void)mlx_data;
 	if ((action == MPRESS) && hbranch->visible && is_inside_comp(hbranch, pos))
 	{
-		hbranch->scrollbox._current_pos -= hbranch->scrollbox.sensitivity;
-		if (hbranch->scrollbox._current_pos < 0)
-			hbranch->scrollbox._current_pos = 0;
+		hbranch->scrollbox._current_pos += hbranch->scrollbox.sensitivity;
+		if (hbranch->scrollbox._current_pos
+			> (hbranch->scrollbox._scroll_buffer.height - hbranch->size.y))
+			hbranch->scrollbox._current_pos
+				= hbranch->scrollbox._scroll_buffer.height - hbranch->size.y;
 		update_scrollbar(hbranch);
 	}
 }
